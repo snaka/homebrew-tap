de5d2536956149de8b1b19738d1d9942a9458cda
snaka (release CI) <snaka@hey.com>
2026-05-12 14:27:45 +0000

    chore: bump bokashi to 0.6.0

--- Casks/bokashi.rb
+++ Casks/bokashi.rb
@@ -1,6 +1,6 @@
 cask "bokashi" do
-  version "0.5.0"
-  sha256 "4a04e6999ca3348b6c8e12f92381df011673782a0ae1639cc7cbc2829a83a8c3"
+  version "0.6.0"
+  sha256 "f4d6ed367786cc12c631e4154ca79b0903aa3c8bae6d3349b06749f9b4fc7523"
 
   url "https://github.com/snaka/Bokashi/releases/download/v#{version}/Bokashi-#{version}.dmg"
   name "Bokashi"
