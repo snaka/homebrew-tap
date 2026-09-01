a5eb4e53eae582ececea9c6a92d311344ebc4132
snaka (release CI) <snaka@hey.com>
2026-09-01 23:02:28 +0000

    chore: bump invixray to 0.1.2

--- Casks/invixray.rb
+++ Casks/invixray.rb
@@ -1,6 +1,6 @@
 cask "invixray" do
-  version "0.1.1"
-  sha256 "3d116f5805defed7713d3e980ec2cb297e9cc169b7ed41d326d513d6fd0e0901"
+  version "0.1.2"
+  sha256 "445ffdd4a4d72f01aee2264f65e7117c40cbbb59e9edf0e6a5440c77ad13de4f"
 
   url "https://github.com/snaka/invixray/releases/download/v#{version}/Invixray-#{version}.dmg"
   name "Invixray"
