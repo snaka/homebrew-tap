d47c922862134bd9f622065830d60ffa09cf3e9b
snaka (release CI) <snaka@hey.com>
2026-09-10 22:52:10 +0000

    chore: bump kelpie to 0.1.9

--- Casks/kelpie.rb
+++ Casks/kelpie.rb
@@ -1,6 +1,6 @@
 cask "kelpie" do
-  version "0.1.8"
-  sha256 "aef161b74fd89456e98e2181580386b48de3557e828038c5c6855aa92a8ff15e"
+  version "0.1.9"
+  sha256 "fac8ac78691c26650fad3825caf95139c733697cf50c2f90336fd9bd9f79428d"
 
   url "https://github.com/snaka/kelpie/releases/download/v#{version}/Kelpie-#{version}.dmg"
   name "Kelpie"
