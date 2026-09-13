1632bb5fd2e00f7a552e1e3a5f229fad5dca8715
Shinji Nakamatsu <19329+snaka@users.noreply.github.com>
2026-09-13 18:56:16 +0900

    chore: bump privmask to 0.3.1

--- Formula/privmask.rb
+++ Formula/privmask.rb
@@ -1,9 +1,9 @@
 class Privmask < Formula
   desc "Mask personal information in text before you share it, on device"
   homepage "https://github.com/snaka/privmask"
-  url "https://github.com/snaka/privmask/releases/download/v0.3.0/privmask-0.3.0-macos-universal.tar.gz"
-  version "0.3.0"
-  sha256 "5cc1dece309f62c6baf36a3b5418005d42e5e57fcbd8b566b672f783b539e572"
+  url "https://github.com/snaka/privmask/releases/download/v0.3.1/privmask-0.3.1-macos-universal.tar.gz"
+  version "0.3.1"
+  sha256 "f1d6bd34ccc023681e2f653caea8b9d5b6f82e603c72f44fbc4b386d0c83d844"
   license "MIT"
 
   depends_on macos: :ventura
